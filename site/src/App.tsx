fdc4b9eee3053dbd4b5ccaa3262444f4f4f044ad
tracy-st <55551284+tracy-st@users.noreply.github.com>
2025-10-09 15:38:48 -0400

    Change header title

--- site/src/App.tsx
+++ site/src/App.tsx
@@ -4,7 +4,7 @@ export default function App() {
 	return (
 		<div className="min-h-screen">
 			<header className="mx-auto max-w-screen-xl p-4 flex items-center justify-between">
-				<h1 className="text-xl font-semibold">Exhibition Site</h1>
+				<h1 className="text-xl font-semibold">HAA 150</h1>
 				<nav className="flex gap-4">
 					<Link to="/">Home</Link>
 					<Link to="/exhibition">Exhibition</Link>
