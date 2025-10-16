06f29d70b6baf025e49cd45a9dd07b4ae77093f2
tracy-st <55551284+tracy-st@users.noreply.github.com>
2025-10-16 16:45:40 -0400

    Update Image block header size

--- haa-150-exhibit/src/components/ImageBlock.tsx
+++ haa-150-exhibit/src/components/ImageBlock.tsx
@@ -89,7 +89,7 @@ export function ImageBlock({
                 <path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z" fill="#fff" />
               </svg>
             </div>
-            <div className="text-m mb-4 font-serif">
+            <div className="text-xl mb-4 font-serif">
               <LocaleString>{canvas.label}</LocaleString>
             </div>
             <div className="exhibition-info-block">
