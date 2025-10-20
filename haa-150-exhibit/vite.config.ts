9d0a37bd237df330cba5f59e3fae89c820fc2841
tracy-st <55551284+tracy-st@users.noreply.github.com>
2025-10-20 16:01:56 -0400

    Update vite.config.ts

--- haa-150-exhibit/vite.config.ts
+++ haa-150-exhibit/vite.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import autoprefixer from 'autoprefixer'
 
-
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   base: '/haa-150-exhibit/',
   plugins: [react()],
-}))
+  css: {
+    postcss: {
+      plugins: [autoprefixer()]
+    }
+  }
+})
