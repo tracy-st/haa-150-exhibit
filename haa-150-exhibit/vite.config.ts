d4b0b2540d5ed836300b8dfba811b1142b8d8d53
tracy-st <55551284+tracy-st@users.noreply.github.com>
2025-10-20 15:34:39 -0400

    Update vite.config.ts

--- haa-150-exhibit/vite.config.ts
+++ haa-150-exhibit/vite.config.ts
@@ -3,12 +3,6 @@ import react from '@vitejs/plugin-react'
 
 
 export default defineConfig({
-<<<<<<< Updated upstream:haa-150-exhibit/vite.config.ts
       base: process.env.GITHUB_PAGES ? '/haa-150-exhibit/' : '/',
       plugins: [react()],
  });
-=======
-  base: process.env.GITHUB_PAGES ? '/haa-150-exhibit/' : '/',
-  plugins: [react()],
-})
->>>>>>> Stashed changes:site/vite.config.ts
