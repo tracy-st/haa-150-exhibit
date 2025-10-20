12fb9dd4e616ee2f5fe5a3417c8cecb442f9a02d
tracy-st <55551284+tracy-st@users.noreply.github.com>
2025-10-20 15:59:23 -0400

    Update deploy config

--- haa-150-exhibit/vite.config.ts
+++ haa-150-exhibit/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 
-export default defineConfig({
-      base: process.env.GITHUB_PAGES ? '/haa-150-exhibit/' : '/',
-      plugins: [react()],
- });
+export default defineConfig(({ mode }) => ({
+  base: '/haa-150-exhibit/',
+  plugins: [react()],
+}))
